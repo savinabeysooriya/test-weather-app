0fc8a46a7fc0e63cc7861f4549d3330c5173cadc
savinabeysooriya <savinabeysooriya@gmail.com>
2021-05-17 10:47:37 +0530

    Making separate styles

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
+import "./styles.scss";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+
+import Box from "@material-ui/core/Box";
 
 const SignIn = () => {
   return (
@@ -14,42 +16,25 @@ const SignIn = () => {
         justifyContent="center"
         height="100vh"
       >
-        <Box p={5} width="50%" bgcolor="#267DFF" className="view-left">
+        <Box p={5} width="50%" bgcolor="#267DFF">
           <Container>
-            <Typography
-              variant="h6"
-              style={{ marginTop: 100, color: "#FFFFFF", fontWeight: "bold" }}
-            >
+            <Typography className="title" variant="h6">
               Welcome to MDM
             </Typography>
 
-            <hr
-              style={{
-                height: 6,
-                backgroundColor: "#FFFFFF",
-                marginRight: 300,
-              }}
-            />
+            <hr className="left-line" />
 
             <Typography
               variant="body1"
               align="justify"
-              style={{ marginTop: 40, color: "#FFFFFF" }}
+              className="left-paragraph"
             >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore
               consectetur, neque doloribus, cupiditate numquam dignissimos
               laborum fugiat deleniti? Eum quasi quidem quibusdam doloribus.
             </Typography>
-            <Button
-              variant="outlined"
-              style={{
-                marginTop: 40,
-                borderRadius: 50,
-                color: "#FFFFFF",
-                borderColor: "#FFFFFF",
-              }}
-            >
+            <Button className="left-button" variant="outlined">
               Know More
             </Button>
           </Container>
@@ -62,58 +47,23 @@ const SignIn = () => {
             alignItems="center"
           >
             <div width="100%">
-              <Typography
-                variant="h6"
-                align="center"
-                style={{ fontWeight: "bold", marginTop: 100 }}
-              >
-                Signin
+              <Typography className="right-text" variant="h6" align="center">
+                SignIn
               </Typography>
-              <hr
-                style={{
-                  height: 6,
-                  backgroundColor: "#267DFF",
-                  width: 40,
-                  marginTop: 0,
-                }}
-              />
+              <hr className="right-line" />
 
-              <form
-                style={{
-                  width: "100%",
-                  marginTop: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "70%",
-                  }}
-                >
+              <form className="form">
+                <div className="text-field">
                   <TextField label="Email" margin="normal"></TextField>
                   <TextField label="password" margin="normal"></TextField>
+                
                 </div>
               </form>
-              <div
-                style={{
-                  width: "100%",
-                  marginTop: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
+              <div className="div-right-btn">
                 <Button
                   variant="contained"
                   color="primary"
-                  style={{
-                    backgroundColor: "#267dff",
-                    borderRadius: "50px",
-                    textTransform: "capitalize",
-                    minWidth: "150px",
-                  }}
+                  className="right-btn"
                 >
                   SignIn
                 </Button>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
+import "./styles.scss";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+
+import Box from "@material-ui/core/Box";
 
 const SignUp = () => {
   return (
@@ -14,42 +16,25 @@ const SignUp = () => {
         justifyContent="center"
         height="100vh"
       >
-        <Box p={5} width="50%" bgcolor="#267DFF" className="view-left">
+        <Box p={5} width="50%" bgcolor="#267DFF">
           <Container>
-            <Typography
-              variant="h6"
-              style={{ marginTop: 100, color: "#FFFFFF", fontWeight: "bold" }}
-            >
+            <Typography className="title" variant="h6">
               Welcome to MDM
             </Typography>
 
-            <hr
-              style={{
-                height: 6,
-                backgroundColor: "#FFFFFF",
-                marginRight: 300,
-              }}
-            />
+            <hr className="left-line" />
 
             <Typography
               variant="body1"
               align="justify"
-              style={{ marginTop: 40, color: "#FFFFFF" }}
+              className="left-paragraph"
             >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore
               consectetur, neque doloribus, cupiditate numquam dignissimos
               laborum fugiat deleniti? Eum quasi quidem quibusdam doloribus.
             </Typography>
-            <Button
-              variant="outlined"
-              style={{
-                marginTop: 40,
-                borderRadius: 50,
-                color: "#FFFFFF",
-                borderColor: "#FFFFFF",
-              }}
-            >
+            <Button className="left-button" variant="outlined">
               Know More
             </Button>
           </Container>
@@ -62,37 +47,13 @@ const SignUp = () => {
             alignItems="center"
           >
             <div width="100%">
-              <Typography
-                variant="h6"
-                align="center"
-                style={{ fontWeight: "bold", marginTop: 100 }}
-              >
+              <Typography className="right-text" variant="h6" align="center">
                 SignUp
               </Typography>
-              <hr
-                style={{
-                  height: 6,
-                  backgroundColor: "#267DFF",
-                  width: 40,
-                  marginTop: 0,
-                }}
-              />
+              <hr className="right-line" />
 
-              <form
-                style={{
-                  width: "100%",
-                  marginTop: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "70%",
-                  }}
-                >
+              <form className="form">
+                <div className="text-field">
                   <TextField label="Email" margin="normal"></TextField>
                   <TextField label="password" margin="normal"></TextField>
                   <TextField
@@ -101,23 +62,11 @@ const SignUp = () => {
                   ></TextField>
                 </div>
               </form>
-              <div
-                style={{
-                  width: "100%",
-                  marginTop: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
+              <div className="div-right-btn">
                 <Button
                   variant="contained"
                   color="primary"
-                  style={{
-                    backgroundColor: "#267dff",
-                    borderRadius: "50px",
-                    textTransform: "capitalize",
-                    minWidth: "150px",
-                  }}
+                  className="right-btn"
                 >
                   SignUp
                 </Button>
